9d3fcc85757b71ca2bd55b45e5cc4904e30b4686
MZTECHSTORE <daltonantonio450@gmail.com>
2026-03-11 21:01:11 +0200

    Create Home component with Navbar and description

---
+++ APP/page.js
@@ -0,0 +1,24 @@
+import Navbar from "../components/Navbar"
+
+export default function Home() {
+
+ return (
+  <div>
+
+   <Navbar />
+
+   <div className="p-10">
+
+   <h1 className="text-4xl font-bold mb-6">
+   MZ Football AI Predictor
+   </h1>
+
+   <p className="text-gray-400">
+   Professional AI football predictions platform.
+   </p>
+
+   </div>
+
+  </div>
+ )
+}
