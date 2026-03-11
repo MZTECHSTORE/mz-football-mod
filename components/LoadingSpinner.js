2cddba9cc45c5ad8e4c5fb6f410743449a2b76fe
MZTECHSTORE <daltonantonio450@gmail.com>
2026-03-11 23:17:10 +0200

    Add LoadingSpinner component

---
+++ components/LoadingSpinner.js
@@ -0,0 +1,13 @@
+export default function LoadingSpinner(){
+
+ return(
+
+  <div className="flex justify-center items-center p-10">
+
+   <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-green-400"></div>
+
+  </div>
+
+ )
+
+}
