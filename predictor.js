57a30722e00840879ff998da6e7dd83e3f119ec0
MZTECHSTORE <daltonantonio450@gmail.com>
2026-03-12 10:54:50 +0200

    Refactor prediction logic and remove poisson function

--- predictor.js
+++ predictor.js
@@ -1,52 +1,16 @@
-function poisson(lambda,k){
-
-let fact=1
-
-for(let i=1;i<=k;i++) fact*=i
-
-return (Math.pow(lambda,k)*Math.exp(-lambda))/fact
-
-}
-
 function predictMatch(){
 
-let homeAttack=Math.random()*2
-let awayAttack=Math.random()*2
+const options = [
 
-let homeWin=0
-let draw=0
-let awayWin=0
+"Home Win",
+"Away Win",
+"Draw",
+"Over 2.5",
+"Under 2.5",
+"Both Teams Score"
 
-for(let h=0;h<5;h++){
+]
 
-for(let a=0;a<5;a++){
-
-let p=poisson(homeAttack,h)*poisson(awayAttack,a)
-
-if(h>a) homeWin+=p
-else if(h==a) draw+=p
-else awayWin+=p
-
-}
-
-}
-
-return {
-
-home:homeWin,
-draw:draw,
-away:awayWin
-
-}
-
-}
-
-function bestPrediction(p){
-
-if(p.home>p.draw && p.home>p.away) return "Home Win"
-
-if(p.away>p.home && p.away>p.draw) return "Away Win"
-
-return "Draw"
+return options[Math.floor(Math.random()*options.length)]
 
 }
